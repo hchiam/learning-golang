0db77eea15a77a5fc2c8ad36e487aaad9eff4803
hchiam <hchiam@users.noreply.github.com>
2019-10-22 17:50:31 -0400

    add more

--- advanced.go
+++ advanced.go
@@ -12,4 +12,25 @@ func main() {
 	*/
 	fmt.Println("Hello World")
 	fmt.Println(t.Now())
+	fmt.Println(`
+--multiline
+----string
+------output
+  ______________
+ /   0      0   \
+|    \______/    |
+ \______________/
+`)
+
+	const earthsGravity = 9.80665
+	fmt.Println("const earthsGravity =", earthsGravity)
+
+	var anInteger int = 1 // int8, int16, int32, int64, uint8, unit16, uint32, unit64
+	/*
+		var anUnsignedInteger uint16 = 1
+		var anotherInteger int8
+		var aFloat float32 = 3.14 // there's also complex
+		var aBoolean bool = true
+	*/
+	fmt.Println("anInteger =", anInteger)
 }
